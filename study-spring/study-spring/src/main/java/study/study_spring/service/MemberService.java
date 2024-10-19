22ad797d67a212db975477bb4b26fb4df35c8e13
jjangol <wedgejjangdol@gmail.com>
2024-10-19 20:00:31 +0900

    spring MemberServiceTest

--- study-spring/study-spring/src/main/java/study/study_spring/service/MemberService.java
+++ study-spring/study-spring/src/main/java/study/study_spring/service/MemberService.java
@@ -11,7 +11,11 @@ import static ch.qos.logback.core.joran.spi.ConsoleTarget.findByName;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
